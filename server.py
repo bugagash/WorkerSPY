22b19b17b4d04f8ae16bfa68dd91727ad75a605f
Bogdan <bogdan297@mail.ru>
2025-10-22 07:14:34 +0300

    Multiple connections from one user case fixed (now it's impossible).

--- server.py
+++ server.py
@@ -89,11 +89,6 @@ class ClientDatabase:
             return self.clients_data[client_mac]['connection_history']
         return []
 
-class ClientConnection:
-    """Class for client"""
-    def __init__(self, client_socket: socket.socket, client_mac: str, client_address: tuple[str, str]):
-        info = {}
-
 class Server:
     """"Classs for server realisation."""
     def __init__(self):
@@ -107,7 +102,8 @@ class Server:
         self.server_socket = None
     
     def handle_client(self, client_socket: socket.socket, client_address: tuple[str, str]) -> None:
-        """Function for making separate thread for client interaction.
+        """Function for making 
+        
         ARGS:   client_socket: socket of client,
                 client_address: tuple['IP', 'PORT'] of client
         Return: None
@@ -123,17 +119,15 @@ class Server:
             message = data.decode('utf-8').strip().split(',')
             
             if message[0] == "MAC_ADDRESS":
-                data = client_socket.recv(4096)
+                data = client_socket.recv(1024)
                 message = data.decode('utf-8').strip()
                 client_mac = message
         except Exception as e:
             print(f"\nEncountered [{client_mac}]: [{e}]\n")
             return
 
-        print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Connected: {client_mac} ({client_ip}:{client_port})")
         self.db.create_client(client_mac)
         client_db_data = self.db.get_client_info(client_mac)
-        self.db.update_client_connection(client_mac, client_ip, client_port, 'connected')
         client_info = {
             'socket': client_socket,
             'ip': client_ip,
@@ -142,17 +136,25 @@ class Server:
             'connected_at': datetime.now(),
             'online_before': client_db_data['total_connections'] > 1
         }
+
+        if client_info['mac'] in [client['mac'] for client in self.clients]:
+            deauth_message = "DEAUTH_REQUEST"
+            client_socket.send(deauth_message.encode('utf-8'))
+            print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Attempting multiple connection {client_mac} ({client_ip}:{client_port})")
+            return
         
+        self.db.update_client_connection(client_mac, client_ip, client_port, 'connected')
+        print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Connected: {client_mac} ({client_ip}:{client_port})")
+
         with self.clients_lock:
             self.clients.append(client_info)
 
         try:
-            curr_client_input = None # ["ProcessJSON", "ScreenShot BMP"]
+            client_input_command = None # ["ProcessJSON", "ScreenShot BMP"]
             client_message_chunks = [] # Get client's big data by 4096 bytes chunks if needed
-            curr_client_message_size = None
-            receiving_size = 1 # Receive message by 1 bytes until full Header read
+            client_message_size = None
+            receiving_size = 1 # Receive message by 1 byte until '\n' read
 
-            # Endless messages receiving process
             while self.server_running:
                 client_socket.settimeout(1.0)
 
@@ -163,51 +165,51 @@ class Server:
                         break
                     
                     # Not in receiving process from client
-                    if not curr_client_input:
+                    if not client_input_command:
                         chunk = data.decode('utf-8')
                         client_message_chunks.append(chunk)
                         
                         if chunk == '\n':
-                            message = "".join(client_message_chunks).split(',')
+                            message = "".join(client_message_chunks).split(',') # Expected <"CommandName", "Message size">
                             if message[0] not in ["ProcessJSON", "ScreenShot BMP"]:
-                                print("Unrecognized message from user\n")
+                                print(f"[{datetime.now().strftime('%H:%M:%S')}] Unrecognized message {client_mac} ({client_ip}:{client_port})")
                             else:
-                                curr_client_input = message[0]
-                                curr_client_message_size = int(message[1])
+                                client_input_command = message[0]
+                                client_message_size = int(message[1])
                                 client_message_chunks = []
                                 receiving_size = 4096
 
                     # Receiving Process Information from client
-                    elif curr_client_input == "ProcessJSON":
+                    elif client_input_command == "ProcessJSON":
                         message = data.decode('utf-8')
                         client_message_chunks.append(message)
                         client_message = ''.join(client_message_chunks)
-                        receiving_size = min(4096, curr_client_message_size - len(client_message))
+                        receiving_size = min(4096, client_message_size - len(client_message))
                         
-                        if len(client_message) == curr_client_message_size:
+                        if len(client_message) == client_message_size:
                             filename = client_db_data['mac'].replace(':', '_')
                             self.log_client_message(filename, client_message)
-                            print(f"Received running processes from client {client_mac} ({client_ip}:{client_port})\n")
+                            print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Received running processes from client {client_mac} ({client_ip}:{client_port})\n")
                             
                             client_message_chunks = []
-                            curr_client_input = None
-                            curr_client_message_size = None
+                            client_input_command = None
+                            client_message_size = None
                             receiving_size = 1
 
                     # Receiving screenshot from client
-                    elif curr_client_input == "ScreenShot BMP":
+                    elif client_input_command == "ScreenShot BMP":
                         client_message_chunks.append(data)
                         bytes_read = sum([len(chunk) for chunk in client_message_chunks])
-                        receiving_size = min(4096, curr_client_message_size - bytes_read)
+                        receiving_size = min(4096, client_message_size - bytes_read)
 
-                        if bytes_read == curr_client_message_size:
+                        if bytes_read == client_message_size:
                             filename = client_db_data['mac'].replace(':', '_')
                             self.save_screenshoot(filename, client_message_chunks)
-                            print(f"Received screenshot from client {client_mac} ({client_ip}:{client_port})\n")
+                            print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Received screenshot from client {client_mac} ({client_ip}:{client_port})\n")
                             
                             client_message_chunks = []
-                            curr_client_input = None
-                            curr_client_message_size = None
+                            client_input_command = None
+                            client_message_size = None
                             receiving_size = 1
                 
                 except socket.timeout:
@@ -217,7 +219,7 @@ class Server:
             print("\nClient Disconnected!")
         except OSError as e:
                 if getattr(e, 'winerror', None) != 10038:
-                    print(f"Error with:{client_mac} ({client_ip}:{client_port}) [{e}]")
+                    print(f"\nError with:{client_mac} ({client_ip}:{client_port}) [{e}]")
         except Exception as e:
             print(f"\n[{datetime.now().strftime('%H:%M:%S')}] Error with: {client_mac} ({client_ip}:{client_port}): {e}")
         
@@ -238,6 +240,7 @@ class Server:
 
     def log_client_message(self, client_ID: str, message: str) -> None:
         """Function for logging client's message to log file.
+        
         ARGS:   client_ID: client's identification ID,
                 message: client's message to log
         Return: None
@@ -248,13 +251,14 @@ class Server:
         with open(log_filename, 'a', encoding='utf-8') as log:
             log.write(f"\n[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]\n{message}\n")
 
-    def save_screenshoot(self, client_ID: str, photo_data: list):
+    def save_screenshoot(self, client_mac: str, photo_data: list) -> None:
         """Function for saving client's screen picture
-        ARGS:   client_ID: client's identification ID,
+        
+        ARGS:   client_mac: client's MAC address,
                 photo_data: list of chunks given in bytes received from client
         Return: None
         """
-        screen_filename = f"screen/{client_ID}_[{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}].bmp"
+        screen_filename = f"screen/{client_mac}_[{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}].bmp"
         Path("screen").mkdir(exist_ok=True)
 
         with open(screen_filename, 'wb') as photo:
@@ -378,20 +382,20 @@ class Server:
     def show_client_history(self) -> None:
         """All clients history"""
         print("\n" + "=" * 80)
-        print("Clients history")
-        print("=" * 80)
+        print(f"Clients history. Total: [{len(self.db.clients_data)}] clients.")
+        print("-" * 80)
         
         for i, (_, client_data) in enumerate(self.db.clients_data.items(), 1):
             print(f"{i}. {client_data['mac']}\n")
             print(f"MAC: {client_data['mac']}")
             print(f"IP: {client_data['ip']}:{client_data['port']}")
-            print(f"Status: {client_data.get('status', 'unknown')}")
             print(f"First online: {client_data['first_seen']}")
             print(f"Last online: {client_data['last_seen']}")
+            print(f"Status: {client_data.get('status', 'unknown')}")
             
             if client_data['connection_history']:
-                print(f"\nConnections history (last 5):")
-                for conn in client_data['connection_history'][-5:]:
+                print(f"\nConnections history (last 3):")
+                for conn in client_data['connection_history'][-3:]:
                     print(f"\t+Connected at: {conn['connected_at']}")
                     if conn['disconnected_at']:
                         print(f"\t-Disconnected at: {conn['disconnected_at']}")
@@ -404,6 +408,7 @@ class Server:
 
     def send_to_client(self, client_index: int, message: str) -> None:
         """Send command to particular client.
+        
         ARGS:   client_index: 1...N number of connected client, 
                 message: message to be sent
         Return: None
@@ -425,6 +430,7 @@ class Server:
     
     def send_Command_to_client(self, client_index: int, command_option: int) -> None:
         """Wrap between send_to_client and user. Allows to send coorect codes to manipulate client's behaviour.
+        
         ARGS:   client_index: number of client,
                 command_option: Choosed allowed option to send.
         Return: None
@@ -445,6 +451,7 @@ class Server:
     
     def send_Command_to_all(self, command_option: int) -> None:
         """Same wrap as send_command_to_client.
+        
         ARGS:   command_option: number of command to be sent.
         Return: None
         """
@@ -502,7 +509,7 @@ class Server:
                         if self.server_running:
                             print(f"\nError while handaling client: {e}")
             
-            accept_thread = threading.Thread(target=accept_clients, daemon=True)
+            accept_thread = threading.Thread(target=accept_clients, daemon=True, name="AcceptClients")
             accept_thread.start()
             
             self.menu_loop()
